33e3836ebfe86409aa1083c59574ca47a2ed23ea
jm-avila <jm.avila1309@icloud.com>
2020-12-06 19:02:19 +0100

    memoization: allConstruct

---
+++ memoization/construct/allConstruct/index.js
@@ -0,0 +1,28 @@
+const allConstruct = (target, wordBank, memo = {}) => {
+  //   if (target in memo) return memo[target];
+  if (target === "") return [[]];
+
+  const result = [];
+
+  for (let word of wordBank) {
+    if (target.indexOf(word) === 0) {
+      const suffix = target.slice(word.length);
+      const suffixWays = allConstruct(suffix, wordBank, memo);
+      const targetWays = suffixWays.map((way) => [word, ...way]);
+      result.push(...targetWays);
+    }
+  }
+
+  memo[target] = result;
+  return result;
+};
+
+console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
+console.log(
+  allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+);
+console.log(allConstruct("eeeeeeeeeeeeeeeeeef", ["e", "ee", "eee", "eeeee"]));
