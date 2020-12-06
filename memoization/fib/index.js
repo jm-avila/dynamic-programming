f1065173abc7c3e02eccc209737376407505e38b
jm-avila <jm.avila1309@icloud.com>
2020-12-06 11:13:14 +0100

    memoization: fib

---
+++ memoization/fib/index.js
@@ -0,0 +1,10 @@
+function fib(n, memo = {}) {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+console.log(fib(5));
+console.log(fib(50));
+console.log(fib(500));
