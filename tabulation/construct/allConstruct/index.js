a3a28656984d9f53ac56bf04510ebe8938139686
jm-avila <jm.avila1309@icloud.com>
2020-12-07 11:49:26 +0100

    tabulation: allConsctruct

---
+++ tabulation/construct/allConstruct/index.js
@@ -0,0 +1,37 @@
+const allConstruct = (target, wordBank) => {
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        const newCombinations = table[i].map((subArray) => [...subArray, word]);
+        table[i + word.length].push(...newCombinations);
+      }
+    }
+  }
+
+  return table[target.length];
+};
+
+console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
+console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abdc"]));
+console.log(
+  allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+);
+console.log(
+  allConstruct("eeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+  ])
+);
