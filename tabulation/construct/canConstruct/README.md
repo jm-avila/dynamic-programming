c9e468d9cfdc6112f0cd3d0866d95c9737d7f11c
jm-avila <jm.avila1309@icloud.com>
2020-12-07 11:06:29 +0100

    tabulation: canConstruct

---
+++ tabulation/construct/canConstruct/README.md
@@ -0,0 +1,7 @@
+# canConstruct(target, wordBank)
+
+Write a function **_canConstruct(target, wordBank)_** that accepts a target string and an array of strings.
+
+The function should return a boolean indicating whether or not the target can be constructed by concatenating elements of the wordBank array.
+
+You may reuse elemnts of wordBank as many times as needed.
---
+++ tabulation/construct/canConstruct/index.js
@@ -0,0 +1,33 @@
+const canConstruct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(false);
+  table[0] = true;
+
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i] === true) {
+      for (let word of wordBank) {
+        if (target.slice(i, i + word.length) === word) {
+          table[i + word.length] = true;
+        }
+      }
+    }
+  }
+  return table[target.length];
+};
+
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abdc"]));
+console.log(
+  canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
+);
+console.log(
+  canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+);
+console.log(
+  canConstruct("eeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+  ])
+);
